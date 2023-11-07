97cc97534eccb9f9c006611818a8af35a65605e3
RaphaelCarvalho1 <ra_carvalho@id.uff.br>
2023-11-06 21:22:31 -0300

    trabalho encerrado

--- main.hs
+++ main.hs
@@ -1,10 +1,12 @@
 
+-- funções auxiliares
+
 stringToInt :: String -> Integer
 stringToInt s = read s
 
 -- conta casos
 
-conta [] (z, m ,v) = print((z, m, v))
+conta [] (z, m ,v) = (z, m, v)
 
 conta (vizinho : resto) (z,m,v) = do
     if vizinho == "Z" then (conta resto (z+1, m, v))
@@ -13,12 +15,14 @@ conta (vizinho : resto) (z,m,v) = do
 
 -- função para cada critério
 
+criterio :: String -> (Integer, Integer, Integer) -> [Char]
+
 criterio no (z, m, v) = if no == "M" && v == 3 then "V"
                         else if no == "V" && z >= 1 then "Z"
                         else if no == "V" && v < 2 then "M"
                         else if no == "V" && v > 3 then "M"
                         else if no == "Z" && v == 0 then "M"
-                        else "V" 
+                        else no
 
 
 -- aplicaCriterio no vizinhos = criterio (no (count vizinhos))
@@ -48,28 +52,76 @@ removeX (y: resto) x = if x == y then (removeX resto x) else (y: (removeX resto 
 
 vizinhos [l1, l2, l3] = removeX ((sublista l1 3) ++ (removeSegundo (sublista l2 3)) ++ (sublista l3 3)) "nil"
 
--- iterações
+-- Funções para gerar nova matriz
 
-iteracao2 [] _ = print ""
+mudaLinha :: [String] -> ([String], [String], [String]) -> [String]
 
-iteracao2 (l: resto) [(viz1: resto1), (viz2: resto2), (viz3: resto3)] = do 
-    print (vizinhos [(viz1: resto1), (viz2: resto2), (viz3: resto3)])
-    iteracao2 resto [resto1, resto2, resto3]
+mudaLinha [] _ = []
 
-iteracao [l1, l2] l3 = do
-    iteracao2 l1 [("nil": l3), ("nil": l1), ("nil": l2)]
+mudaLinha (l: resto) ((viz1: resto1), (viz2: resto2), (viz3: resto3)) = 
+    ((criterio l (conta ((vizinhos [(viz1: resto1), (viz2: resto2), (viz3: resto3)])) (0, 0, 0))):
+    mudaLinha resto (resto1, resto2, resto3))
 
-iteracao (l1: (l2: restoListas)) l3 = do
-    iteracao2 l1 [("nil": l3), ("nil": l1), ("nil": l2)]
-    iteracao (l2: restoListas) l1
+mudaMatriz :: [[String]] -> [String] -> [[String]]
+
+mudaMatriz [l1, l2] l3 = [mudaLinha l1 (("nil": l3), ("nil": l1), ("nil": l2))]
+
+mudaMatriz (l1: (l2: restoListas)) l3 =
+    ((mudaLinha l1 (("nil": l3), ("nil": l1), ("nil": l2))):
+    mudaMatriz (l2: restoListas) l1)
+
+-- Iteração base do programa
+
+itera matriz _ 0 = (matriz, 0)
+
+itera matriz dim n = do
+    let res = mudaMatriz (matriz ++ [(crianil [] dim)]) (crianil [] dim)
+    if res == matriz then (res, n)
+    else (itera res dim (n-1))
+
+-- Interface
+
+quebraLinha :: String -> [String]
+
+quebraLinha [] = []
+
+quebraLinha (l:resto) = if l == ' ' then quebraLinha resto
+                        else [[l]] ++ (quebraLinha resto)
+
+lerLinha :: Integer -> IO [String]
+
+lerLinha n = do    
+    print ("Escreva a linha " ++ (show n) ++ ":")
+    palavra <- getLine
+    let linha = quebraLinha palavra
+    return linha
+
+printaResultado :: ([[String]], Integer) -> Integer -> IO()
+
+printaResultado (matriz, i) n = do
+    print matriz
+    if i == 0 then do
+        putStrLn "O sistema não estabilizou"
+    else do
+        putStrLn ("Estabilizou após "++ (show (n-i)) ++ " iterações")
+
+leMatrizEExecuta :: Integer -> Integer -> Integer -> [[String]] -> IO()
+
+leMatrizEExecuta dim n i matriz = do
+    if dim == i then do
+        printaResultado (itera matriz dim n) n
+    else do
+        linha <- (lerLinha i)
+        leMatrizEExecuta dim n (i+1) (matriz++[linha])
+    
 
 main :: IO()
 
-main = do
-    n <- getLine
-    let h = stringToInt n
-    (iteracao [["V", "Z", "M", "M"], 
-                ["M", "M", "M", "Z"], 
-                ["Z", "V", "Z", "V"], 
-                ["M", "V", "M", "Z"],
-                (crianil [] h)] (crianil [] h))
+main = do 
+    putStrLn "Digite a dimensão da matriz: "
+    m <- getLine
+    let dim = stringToInt m
+    putStrLn "Digite a quantidade máxima de iterações: "
+    nString <- getLine
+    let n = stringToInt nString
+    leMatrizEExecuta dim n 0 []
